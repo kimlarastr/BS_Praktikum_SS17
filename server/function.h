e7f9f716b13a94b3c43c5c0fa056718cbc23c847
Julia Voell <julia_elisabeth.voell@smail.th-koeln.de>
2017-05-29 13:55:28 +0200

    Alex hat das data in function.h verlegt

--- server/function.h
+++ server/function.h
@@ -7,3 +7,10 @@ int put(char* key, char* value, char* res);
 int get(char* key, char* res);
 
 int del(char* key, char* res);
+
+struct data{
+  char key[30];
+  char value[30];
+};
+
+struct data daten[N];
